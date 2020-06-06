e3838dd03ec418db2ca0799b7f64076908c3418c
yyffhh22 <yyffhh0522@gmail.com>
2020-06-06 13:05:50 +0800

    add interaction

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -3,4 +3,18 @@ describe ('Test App', () => {
     it ('launches', () => {
       cy.visit ('/');
     });
+
+    it ('open the app with the login button', () => {
+        cy.visit('/');
+        cy.contains('Sign in with Google');
+    });
+
+    it('user can click the "add a new goal" to create a new goal card', () => {
+        cy.visit ('/');
+        cy.contains('Sign in with Google').click();
+        cy.contains('Add a new goal').click();
+        cy.contains('Create a new goal');
+        cy.contains('Submit');
+        cy.contains('Cancel');
+        });
   });
